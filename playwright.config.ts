93806cbc8b26abfbef4a45f18a05bb17af07c0bf
Irina Dimitriu <lutz.sharlota13@gmail.com>
2025-11-28 14:48:41 +0200

    Add repo

---
+++ playwright.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig, devices } from "@playwright/test";
+
+const IS_CI = !!process.env["CI"];
+
+export default defineConfig({
+  testDir: "./e2e",
+  testMatch: "*.spec.ts",
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: IS_CI,
+  /* Retry on CI only */
+  retries: IS_CI ? 1 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: IS_CI ? 2 : undefined,
+  // workers: 2,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [
+    [
+      "html",
+      {
+        outputFolder: "./playwright-report/index.html",
+        open: IS_CI ? "never" : "on-failure",
+      },
+    ],
+    ["blob"],
+    ["github"],
+    ["allure-playwright"],
+  ],
+
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://127.0.0.1:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: "on-first-retry",
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+});
